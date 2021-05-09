6038dac0357d011eeada38e19cd4001e15699c72
Areena Akhter <akhterareena@gmail.com>
2021-05-09 18:46:25 -0400

    updated search page styling

--- frontend/src/components/PosterGallery/PosterGallery.styles.js
+++ frontend/src/components/PosterGallery/PosterGallery.styles.js
@@ -30,7 +30,6 @@ export const SectionSubtitle = styled.h4`
     text-align: center;
     justify-content: center;
     color: #FFFFFF;
-    padding-bottom: 20px;
 `
 
 export const PosterGalleryContainer = styled.div`
@@ -38,8 +37,8 @@ export const PosterGalleryContainer = styled.div`
     justify-content: center;
     align-items: center;
     width: 100%;
-    padding-top: 3rem;
-    margin-top: 5rem;
+    padding-top: 1rem;
+    margin-top: 2rem;
     float: left,
 `;
 
